b42f0eb9dae4e49e9dbeb43433da696659991309
BerkcanB <berkcan3514@gmail.com>
2021-06-03 22:36:56 +0300

    first commit

--- diyetUygulamasi/Form1.cs
+++ diyetUygulamasi/Form1.cs
@@ -14,7 +14,7 @@ namespace diyetUygulamasi
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();//Yorum Satırı commit deneme
         }
     }
 }
